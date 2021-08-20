7463d5100be65f4e391eab3bb720eec064efda1b
João Danilo <jddiedrich@gmail.com>
2021-08-20 20:04:51 -0300

    Adionada listagem de Produtos

--- src/main/java/compasso/bolsa/dao/ProdutoDAO.java
+++ src/main/java/compasso/bolsa/dao/ProdutoDAO.java
@@ -1,7 +1,6 @@
 package compasso.bolsa.dao;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -9,34 +8,32 @@ import javax.persistence.EntityManager;
 import compasso.bolsa.model.Produto;
 
 public class ProdutoDAO {
-    
+
     private EntityManager entityManager;
 
-    public ProdutoDAO(EntityManager entityManager){
+    public ProdutoDAO(EntityManager entityManager) {
         this.entityManager = entityManager;
     }
-    
-    public Produto atualizar(Produto produto){
+
+    public Produto atualizar(Produto produto) {
         produto.setDataAlteracao(LocalDate.now());
         return entityManager.merge(produto);
     }
 
-    public void cadastrar(Produto produto){
+    public void cadastrar(Produto produto) {
         entityManager.persist(produto);
     }
 
-    public void deletar(Produto produto){
+    public void deletar(Produto produto) {
         produto = entityManager.merge(produto);
         entityManager.remove(produto);
     }
 
-    public List<Produto> listar(){
-        List<Produto> produtos = new ArrayList<Produto>();
-
-        return produtos;
+    public List<Produto> listar() {
+        return entityManager.createQuery("SELECT p FROM Produto", Produto.class).getResultList();
     }
-    
-    public void limparTabela(){
+
+    public void limparTabela() {
         entityManager.createQuery("DELETE FROM Produto").executeUpdate();
     }
 
